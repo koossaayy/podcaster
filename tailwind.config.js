266de583a6b1aa966a6c9b9d780a2bbf393bd10a
Koussay <koussay@prettify.studio>
2022-02-20 22:03:58 +0100

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        darkviolet: '#09002b',
+        lightviolet: '#1c0030',
+        lightindigo: '#4303A4',
+        'dark-blue': '#11024B',
+        start: '#3531A9',
+        end: '#4303A4',
+        'button-start': '#C157AB',
+        'button-end': '#FB85A1',
+        'talk-to-us-button': '#3531A9',
+        'footer-background': '#E5E5E5'
+      }
+    },
+    fontFamily: {
+      'small': ['Nunito Sans', 'system-ui'],
+     
+    }
+  },
+  plugins: [],
+}
